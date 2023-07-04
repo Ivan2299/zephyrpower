c0b1f9f628b6c5cdae716a4887387c4fb0b25b0e
Ivan Kovba <kovbaivan11112@gmail.com>
2023-07-04 17:14:36 +0300

    slider new doing

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Loaded');
 
+	// <sliders>
 	const verrticalSliderBanner = document.querySelector('.vertical-slider');
 	const circleSlider = document.querySelector('.circle-slider');
+	const sliderNew = document.querySelector('.new-slider');
 
 	let slider1 = new Swiper(verrticalSliderBanner, {
 		direction: 'vertical',
@@ -14,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		centeredSlides: true,
 		slidesPerView: 2,
 		loop: true,
-		// spaceBetween: -70,
+		spaceBetween: -70,
 		// grabCursor: true,
 		// slideToClickedSlice: true,
 		slidesPerGroup: 1,
@@ -98,4 +100,50 @@ document.addEventListener('DOMContentLoaded', function () {
 		// 	},
 		// },
 	});
+
+	let slider3 = new Swiper(sliderNew, {
+		direction: 'horizontal',
+		// // paralax: true,
+		// observer: true,
+		// observeParents: true,
+		// watchOverflow: true,
+		centeredSlides: true,
+		slidesPerView: 3,
+		// spaceBetween: 50,
+		// // grabCursor: true,
+		// // slideToClickedSlice: true,
+		slidesPerGroup: 1,
+		// // autoHeight: true,
+		// speed: 800,
+		// effect: 'slide',
+		// // autoplay: {
+		// 	delay: 1000,
+		// },
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// breakpoints: {
+		// 	640: {
+		// 		slidesPerView: 1,
+		// 		spaceBetween: 0,
+		// 		autoHeight: true,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	1268: {
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 30,
+		// 	},
+		// },
+	});
+	// </sliders>
 });
