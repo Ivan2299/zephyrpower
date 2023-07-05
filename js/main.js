6384a7fc9f109d17f5e48fe0624078625c765aca
Ivan Kovba <kovbaivan11112@gmail.com>
2023-07-05 19:05:20 +0300

    bestsellers done

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Loaded');
 
+	// select
+	$(document).ready(function () {
+		$('#mySelect').select2();
+	});
+
 	// <sliders>
 	const verrticalSliderBanner = document.querySelector('.vertical-slider');
 	const circleSlider = document.querySelector('.circle-slider');
 	const sliderNew = document.querySelector('.new-slider');
+	const sliderBestsellers = document.querySelector('.bestsellers-slider');
 
 	let slider1 = new Swiper(verrticalSliderBanner, {
 		direction: 'vertical',
@@ -104,14 +110,15 @@ document.addEventListener('DOMContentLoaded', function () {
 	let slider3 = new Swiper(sliderNew, {
 		direction: 'horizontal',
 		// paralax: true,
-		observer: true,
-		observeParents: true,
-		watchOverflow: true,
+		// observer: true,
+		// observeParents: true,
+		// watchOverflow: true,
 		centeredSlides: true,
 		slidesPerView: 3,
-		// spaceBetween: 50,
+		initialSlide: 2,
+		spaceBetween: 20,
 		// // grabCursor: true,
-		// // slideToClickedSlice: true,
+		slideToClickedSlice: false,
 		slidesPerGroup: 1,
 		// autoHeight: true,
 		speed: 800,
@@ -156,5 +163,60 @@ document.addEventListener('DOMContentLoaded', function () {
 		// 	},
 		// },
 	});
-	// </sliders>
+	let slider4 = new Swiper(sliderBestsellers, {
+		direction: 'horizontal',
+		// paralax: true,
+		// observer: true,
+		// observeParents: true,
+		// watchOverflow: true,
+		centeredSlides: true,
+		loop: true,
+		slidesPerView: 3,
+		initialSlide: 1,
+		spaceBetween: 40,
+		// // grabCursor: true,
+		slideToClickedSlice: false,
+		slidesPerGroup: 1,
+		// autoHeight: true,
+		speed: 800,
+		effect: {
+			slide: {
+				// Slide effect options
+				shadow: true, // Add shadow to the slides
+				fadeEffect: {
+					crossFade: true, // Cross-fade transition effect
+				},
+			},
+		},
+
+		// autoplay: {
+		// 	delay: 4000, // Delay between slide transitions (in milliseconds)
+		// 	disableOnInteraction: false, // Autoplay continues even when the user interacts with the slider
+		// },
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// breakpoints: {
+		// 	640: {
+		// 		slidesPerView: 1,
+		// 		spaceBetween: 0,
+		// 		autoHeight: true,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	1268: {
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 30,
+		// 	},
+		// },
+	});
 });
