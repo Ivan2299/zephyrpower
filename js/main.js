8b04805a327295fc4c2e6199eb6eeba4788c86e0
Ivan Kovba <kovbaivan11112@gmail.com>
2023-07-11 18:08:09 +0300

    reviews almost finish

--- js/main.js
+++ js/main.js
@@ -2,16 +2,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Loaded');
 
-	// searchInput
-	const searchId = document.querySelector('.header__search');
-	searchId.addEventListener('click', function (e) {
-		document.body.classList.add('search-open');
+	// Search
+	const body = document.getElementsByTagName('body')[0];
+	const searchInput = document.querySelector('.header__search-input');
+	searchInput.addEventListener('focus', event => {
+		// event.target.parentElement.classList.add('active');
+		body.classList.add('search-open');
+	});
+
+	searchInput.addEventListener('blur', event => {
+		// event.target.parentElement.classList.remove('active');
+		// body.classList.remove('search-open');
 	});
 
 	// select
-	$(document).ready(function () {
-		$('#mySelect').select2();
-	});
+	// $(document).ready(function () {
+	// 	$('#mySelect').select2();
+	// });
 
 	// sliders
 	const verrticalSliderBanner = document.querySelector('.vertical-slider');
@@ -19,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const sliderNew = document.querySelector('.new-slider');
 	const sliderBestsellers = document.querySelector('.bestsellers-slider');
 	const sliderPower = document.querySelector('.power-slider');
+	const sliderSpecial = document.querySelector('.special-slider');
 
 	let slider1 = new Swiper(verrticalSliderBanner, {
 		direction: 'vertical',
@@ -233,6 +241,61 @@ document.addEventListener('DOMContentLoaded', function () {
 		// // // grabCursor: true,
 		// slideToClickedSlice: false,
 		// slidesPerGroup: 1,
+		// autoHeight: true,
+		speed: 800,
+		effect: {
+			slide: {
+				// Slide effect options
+				shadow: true, // Add shadow to the slides
+				fadeEffect: {
+					crossFade: true, // Cross-fade transition effect
+				},
+			},
+		},
+
+		// autoplay: {
+		// 	delay: 4000, // Delay between slide transitions (in milliseconds)
+		// 	disableOnInteraction: false, // Autoplay continues even when the user interacts with the slider
+		// },
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// breakpoints: {
+		// 	640: {
+		// 		slidesPerView: 1,
+		// 		spaceBetween: 0,
+		// 		autoHeight: true,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 3,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	1268: {
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 30,
+		// 	},
+		// },
+	});
+	let slider6 = new Swiper(sliderSpecial, {
+		direction: 'horizontal',
+		// paralax: true,
+		// observer: true,
+		// observeParents: true,
+		// watchOverflow: true,
+		centeredSlides: true,
+		loop: true,
+		slidesPerView: 3,
+		initialSlide: 1,
+		// // grabCursor: true,
+		slideToClickedSlice: true,
+		slidesPerGroup: 1,
 		// autoHeight: true,
 		speed: 800,
 		effect: {
